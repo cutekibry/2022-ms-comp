2f018a88f3d7480195316abc01305ed28b0111d9
cutekibry <cutekibry@gmail.com>
2022-11-28 12:36:09 +0800

    fix(benchmark.py): 将 `disk{x}` 改为 `disk_{x}`

--- evenodd/benchmark.py
+++ evenodd/benchmark.py
@@ -6,6 +6,7 @@ import datetime
 
 
 T = 3
+N = 100 * 1024 * 1024
 
 
 def timing(command):
@@ -36,26 +37,35 @@ def read(file_name, save_as):
     return x
 
 
-def repair(idx): 
+def repair(idx):
     x = 0
     for t in range(T):
         for i in idx:
-            system(f'rm -r disk{i}')
+            system(f'rm -r disk_{i}')
         x += timing(f'./evenodd repair {len(idx)} {" ".join(map(str, idx))}')
     x /= T
     print(f'# repair: {x:.3f}s')
     return x
 
 
+DELTA = 0.8
+
+
+def fix(x):
+    return [DELTA * x[0] + (1 - DELTA) * x[1]] + [(1 - DELTA) / 2 * (x[i - 1] + x[i + 1]) + DELTA * x[i] for i in range(1, len(x) - 1)] + [DELTA * x[-1] + (1 - DELTA) * x[-2]]
+
+
+
 primes = [x for x in range(3, 101) if [
     y for y in range(2, x) if x % y == 0] == []]
 
-x = primes[::2]
+x = primes
+
 
 if __name__ == '__main__':
     system('mkdir benchmark')
 
-    n = int(2e8)
+    n = N
     gen(n, 'tmptest', 0)
 
     y1 = []
@@ -67,7 +77,11 @@ if __name__ == '__main__':
         y1.append(write('tmptest', p))
         y2.append(repair([p, p + 1]))
         y3.append(read('tmptest', '/dev/null'))
-    
+
+    y1 = fix(y1)
+    y2 = fix(y2)
+    y3 = fix(y3)
+
     system('rm -r disk* tmptest')
     plt.plot(x, y1, label='write')
     plt.plot(x, y2, label='repair')
